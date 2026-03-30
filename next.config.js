acfe8c149663b746946048f821c4193eb2749adf
pranayx77 <pranaydubru@gmail.com>
2026-03-30 15:38:59 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'coin-images.coingecko.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.coingecko.com',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
